9d9269bbc444e8228d0298d1b99380733f4ca358
fuadsuleyman <fuadsuleymanov@outlook.com>
2022-06-18 15:06:51 +0400

    redirect / to /teams route

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import TeamMembers from './components/teams/TeamMembers';
 const router = createRouter({
   history: createWebHistory(),
   routes: [
+    { path: '/', redirect: '/teams' },
     { path: '/teams', component: TeamsList },
     { path: '/users', component: UsersList },
     { path: '/teams/:teamId', component: TeamMembers, props: true },
