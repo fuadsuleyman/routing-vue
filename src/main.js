92617fae326d3e14f0fae2ce280f4a5a1785ef23
fuadsuleyman <fuadsuleymanov@outlook.com>
2022-06-18 15:10:08 +0400

    I use alias alternative to redirect but in this case url not change

--- src/main.js
+++ src/main.js
@@ -10,8 +10,8 @@ import TeamMembers from './components/teams/TeamMembers';
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', redirect: '/teams' },
-    { path: '/teams', component: TeamsList },
+    // { path: '/', redirect: '/teams' },
+    { path: '/teams', component: TeamsList, alias: '/' },
     { path: '/users', component: UsersList },
     { path: '/teams/:teamId', component: TeamMembers, props: true },
   ],
