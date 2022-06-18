e149d22396825fb931b5bf9b5d9cc2564442423d
fuadsuleyman <fuadsuleymanov@outlook.com>
2022-06-18 18:56:51 +0400

    add guard but now in comments, it prevent to enter to users

--- src/main.js
+++ src/main.js
@@ -39,6 +39,16 @@ const router = createRouter({
     return { left: 0, top: 0 };
   },
 });
+router.beforeEach(function (to, from, next) {
+  console.log('Global beforeEach');
+  console.log(to, from);
+  // if (to.name === 'team-members') {
+  //   next();
+  // } else {
+  //   next({ name: 'team-members', params: { teamId: 't2' } });
+  // }
+  next();
+});
 
 const app = createApp(App);
 
