9f6e283fdb857e458b50b270b0f2d9857db4280f
fuadsuleyman <fuadsuleymanov@outlook.com>
2022-06-18 18:12:02 +0400

    add named routers similar to slots default and named

--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ import TeamsList from './components/teams/TeamsList';
 import UsersList from './components/users/UsersList';
 import TeamMembers from './components/teams/TeamMembers';
 import NotFound from './components/nav/NotFound';
+import TeamsFooter from './components/teams/TeamsFooter';
+import UsersFooter from './components/users/UsersFooter';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -15,12 +17,17 @@ const router = createRouter({
     {
       name: 'teams',
       path: '/teams',
-      component: TeamsList,
+      components: { default: TeamsList, footer: TeamsFooter },
       children: [
-        { name:'team-members', path: '/teams/:teamId', component: TeamMembers, props: true },
+        {
+          name: 'team-members',
+          path: '/teams/:teamId',
+          component: TeamMembers,
+          props: true,
+        },
       ],
     },
-    { path: '/users', component: UsersList },
+    { path: '/users', components: {default:UsersList, footer: UsersFooter }},
 
     { path: '/:notFound(.*)', component: NotFound },
   ],
